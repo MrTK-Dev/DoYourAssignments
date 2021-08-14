d044a6fd827e02243f0e2f079dd2296529f5425f
saltyMrTK <Dev.MrTK@gmail.com>
2021-08-14 14:53:43 +0200

    New converter to toggle Visibility :mag:

--- Scripts/DataConverter.cs
+++ Scripts/DataConverter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace DYA.Converters
@@ -26,6 +27,19 @@ namespace DYA.Converters
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             return null;
+        }
+    }
+
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
         }
     }
 }
