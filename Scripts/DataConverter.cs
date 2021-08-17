61611ae6aa60786ee3881c244f4b3d99273f909b
saltyMrTK <Dev.MrTK@gmail.com>
2021-08-17 09:57:09 +0200

    Minor code cleanup

--- Scripts/DataConverter.cs
+++ Scripts/DataConverter.cs
@@ -30,16 +30,16 @@ namespace DYA.Converters
         }
     }
 
-    public class VisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return (Visibility)value == Visibility.Visible;
-        }
+    //public class VisibilityConverter : IValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    //    {
+    //        return (Visibility)value == Visibility.Visible;
+    //    }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
-        }
-    }
+    //    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    //    {
+    //        return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+    //    }
+    //}
 }
